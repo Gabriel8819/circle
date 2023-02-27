f2e16ce2424e3a703722c764fc8630aecf0dde29
gabriel boyer-Beaulieu <gabriel8819@hotmail.com>
2023-02-27 12:13:41 -0500

    add an echo method

--- src/test/java/com/pyxelart/PrinterTest.java
+++ src/test/java/com/pyxelart/PrinterTest.java
@@ -5,18 +5,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class PrinterTest {
+	
+	
+	Printer printer = new Printer();
 
 	
 	@Test
 	void givenAPrinter_whenEnterAValue_PrintTHeValue() {
-		Printer printer = new Printer();
 		String result = printer.print("Hello World");
 		String expected = "Printing... Hello World";
 		assertEquals(expected, result);
-		
-		
 	}
 	
+	@Test
+	void givenAPrinter_whenEnterAValue_EchoTheValue() {
+		String result = printer.echo("Hello World");
+		String expected = "Echo... Hello World";
+		assertEquals(expected, result);
+	}
 	
 	
 }
